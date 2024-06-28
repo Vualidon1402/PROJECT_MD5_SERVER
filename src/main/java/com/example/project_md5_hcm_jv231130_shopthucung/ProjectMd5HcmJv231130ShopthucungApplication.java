fff0cbcc9cd685d4c6459763287d5fa71b59603f
Vualidon1402 <nguyendanhkien1402@gmai.com>
2024-06-27 19:41:49 -0700

    Khung Project

---
+++ src/main/java/com/example/project_md5_hcm_jv231130_shopthucung/ProjectMd5HcmJv231130ShopthucungApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_md5_hcm_jv231130_shopthucung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMd5HcmJv231130ShopthucungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMd5HcmJv231130ShopthucungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_md5_hcm_jv231130_shopthucung/ProjectMd5HcmJv231130ShopthucungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_md5_hcm_jv231130_shopthucung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMd5HcmJv231130ShopthucungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
