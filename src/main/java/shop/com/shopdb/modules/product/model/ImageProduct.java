471af0fcba31fd8906197ab3f7d83d60ac7420fa
unknown <quangtrungdn1994@gmail.com>
2024-07-05 09:10:41 +0700

    product

---
+++ src/main/java/shop/com/shopdb/modules/product/model/ImageProduct.java
@@ -0,0 +1,25 @@
+package shop.com.shopdb.modules.product.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class ImageProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer image_id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
